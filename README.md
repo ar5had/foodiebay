bcefaf59b99d07dd7c482a9b03ac91fd54c546b7
Arshad Khan <arshdkhn1@gmail.com>
2016-12-24 21:31:39 +0000

    final styling updates

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ A full stack JavaScript app for nightlife coordination.
 
 Todos:
 
-1- Make design mobile friendly
+~~ 1- Make design mobile friendly ~~
 
 ~~ 2- Find a way to make req to results page with query when user logs in . Get rid of session variable. ~~
 
--- public/javascripts/homepage.js
+++ public/javascripts/homepage.js
@@ -1,52 +1,52 @@
 (function(){
 
-var elem = document.querySelector('.locateMe');
-var form = document.getElementById('locateMeForm');
-var userABWrapper = document.querySelector("header .buttonWrapper > a");
-var userAccBtn = document.querySelector('.userAccBtn');
-
-elem.addEventListener("click", function() {
-    form.submit();    
-});
-
- userAccBtn.addEventListener('click', function(e) {
-        if (!e) var e = window.event
-    		e.cancelBubble = true;
-    	if (e.stopPropagation) 
-    		e.stopPropagation()
-    		
-    	if(userABWrapper.style.visibility === "visible")	
-            userABWrapper.style.visibility = "hidden";
-        else
-            userABWrapper.style.visibility = "visible";
+    var elem = document.querySelector('.locateMe');
+    var form = document.getElementById('locateMeForm');
+    var userABWrapper = document.querySelector("header .buttonWrapper > a");
+    var userAccBtn = document.querySelector('.userAccBtn');
+    
+    elem.addEventListener("click", function() {
+        form.submit();    
     });
     
-   
-    
-    window.addEventListener("click", function() {
-        if (window.innerWidth <= 550)
-            userABWrapper.style.visibility = "hidden";
-            
-    });
-    
-    
-    if(window.attachEvent) {
-    window.attachEvent('onresize', function() {
-            if(window.innerWidth > 550) {
-                userABWrapper.style.visibility = "visible";
-            } else {
+     userAccBtn.addEventListener('click', function(e) {
+            if (!e) var e = window.event
+        		e.cancelBubble = true;
+        	if (e.stopPropagation) 
+        		e.stopPropagation()
+        		
+        	if(userABWrapper.style.visibility === "visible")	
                 userABWrapper.style.visibility = "hidden";
-            }
-    });
-    }
-    else if(window.addEventListener) {
-        window.addEventListener('resize', function() {
-            if(window.innerWidth > 550) {
+            else
                 userABWrapper.style.visibility = "visible";
-            } else {
+        });
+        
+       
+        
+        window.addEventListener("click", function() {
+            if (window.innerWidth <= 550)
                 userABWrapper.style.visibility = "hidden";
-            }
-        }, true);
-    }
+                
+        });
+        
+        
+        if(window.attachEvent) {
+        window.attachEvent('onresize', function() {
+                if(window.innerWidth > 550) {
+                    userABWrapper.style.visibility = "visible";
+                } else {
+                    userABWrapper.style.visibility = "hidden";
+                }
+        });
+        }
+        else if(window.addEventListener) {
+            window.addEventListener('resize', function() {
+                if(window.innerWidth > 550) {
+                    userABWrapper.style.visibility = "visible";
+                } else {
+                    userABWrapper.style.visibility = "hidden";
+                }
+            }, true);
+        }
     
 })();
